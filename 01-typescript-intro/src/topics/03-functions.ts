cf6eeaf0556b80e8b1e79da207db98a84cfeadb6
Jonathan Sánchez <jonathan.sanchez9@zurich.com>
2024-04-14 20:52:07 -0500

    learn-functions-w-interfaces

--- 01-typescript-intro/src/topics/03-functions.ts
+++ 01-typescript-intro/src/topics/03-functions.ts
@@ -10,11 +10,34 @@ function multiply(first: number, second?: number, base: number = 2) {
     return first * base;
 }
 
-const result = addNumber(1,1); 
-const resultArrow = addNumberArrow(1,1); 
-const multiplyResult = multiply(5);
+// const result = addNumber(1,1); 
+// const resultArrow = addNumberArrow(1,1); 
+// const multiplyResult = multiply(5);
 
-console.log({ result, resultArrow, multiplyResult });
+// console.log({ result, resultArrow, multiplyResult });
+
+interface Character {
+    name: string;
+    hp: number;
+    showHp: () => void;
+}
+
+const healCharacter = (character: Character, amount: number) => {
+ character.hp += amount;
+}
+
+const jonathan: Character = {
+    name: 'Jonathan',
+    hp: 50,
+    showHp() {
+        console.log(`Puntos de vida: ${this.hp}`)
+    }
+}
+
+healCharacter(jonathan, 25);
+healCharacter(jonathan, 25);
+
+jonathan.showHp();
 
 
 export {};
