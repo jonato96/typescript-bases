e3469614b38cc70c5e7dd8e23e01662d7fdf8348
Jonathan Sánchez <jonathan.sanchez9@zurich.com>
2024-04-15 22:19:42 -0500

    learn-extends

--- 01-typescript-intro/src/topics/08-classes.ts
+++ 01-typescript-intro/src/topics/08-classes.ts
@@ -9,6 +9,22 @@ export class Person {
 
 }
 
+export class Hero extends Person {
+
+    constructor(
+        public alterEgo: string,
+        public age: number,
+        public realName: string
+    ) {
+        super(realName, 'NY');
+    }
+
+}
+
 const ironman = new Person('Jona', 'Ec');
+
+const cap = new Hero('Captain America', 100, 'Evans');
+
 console.log(ironman)
+console.log(cap)
 
